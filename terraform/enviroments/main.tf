beef91761d57505a3f778019b743275ebc869805
RoadBlack <patrykmroczek10@gmail.com>
2026-05-08 16:08:31 +0200

    Add secure Azure Storage Account to main.tf

--- terraform/enviroments/main.tf
+++ terraform/enviroments/main.tf
@@ -10,7 +10,22 @@ resource "azurerm_resource_group" "production" {
   name     = "rg-production"
   location = var.region
   tags = {
-    environment = "production"
+    environment = "production-resource-group"
   }
 
+}
+
+resource "azurerm_storage_account" "production_storage_account" {
+  name = "production-storage"
+  resource_group_name = azurerm_resource_group.production.name
+  location = var.region
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  access_tier = "Hot"
+  allow_nested_items_to_be_public = false
+  https_traffic_only_enabled = true
+  min_tls_version = 1.2
+  tags = {
+    environment = "production-storage"
+  }
 }
