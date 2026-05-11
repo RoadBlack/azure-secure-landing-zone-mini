5973aa6cc0326ea35220e503f97802b34d9d7f1b
RoadBlack <patrykmroczek10@gmail.com>
2026-05-11 12:20:24 +0200

    fixing production subnet CIDR

--- terraform/enviroments/network.tf
+++ terraform/enviroments/network.tf
@@ -8,7 +8,7 @@ resource "azurerm_subnet" "production_subnet" {
   name                 = "production-subnet"
   resource_group_name  = azurerm_resource_group.production.name
   virtual_network_name = azurerm_virtual_network.production_vnet.name
-  address_prefixes     = ["10.0.0.1/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 resource "azurerm_network_security_group" "production_nsg" {
   name                = "production-nsg"
